a0fb415219b365f2058b01c56505ad939752959f
ilyamirin <ilya.mirin@gmail.com>
2020-03-31 16:13:23 +1000

    fix logging

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import telebot
 import nltk
 import random
 import wget
-
+import re
 import logging
-logging.basicConfig(filename="savior.log", level=logging.INFO)
+
+logging.basicConfig(filename="savior.log", level=logging.INFO, format='%(asctime)s  %(name)s  %(levelname)s: %(message)s')
 
 path = '/Users/ilyamirin/Projects/lang_savior/voices/'
 token = '1132212407:AAErGfUDqfnSiZ5Qz_1OVYTqxKfPE6uLmG8' 
@@ -14,7 +15,8 @@ sentences = []
 nltk.download('punkt')
 
 
-for txt in ["./voyna-i-mir-tom-1.txt", 'petushki.txt']:
+
+for txt in ["voyna-i-mir-tom-1.txt", 'petushki.txt']:
     f = open(txt, "r")
     if f.mode == 'r':
         contents = f.read()
@@ -26,8 +28,7 @@ for txt in ["./voyna-i-mir-tom-1.txt", 'petushki.txt']:
     i = 1024 * 2
     while i > 0:
         line = a_list[random.randint(10, len(a_list)-1)]
-        if len(line) <= 255 & len(line) > 29:
-            #print(line)
+        if re.match('(?!–\s)(?!\()(?!\d).{29,255}(?!\S{2}\.)', line):
             sentences.append(line)
             i -= 1
 
@@ -38,18 +39,16 @@ bot = telebot.TeleBot(token)
 def add_phrase(message):
     bot.send_message(message.chat.id, 'Дорогой %(username)s, ответь на сообщение голосовым с фразой:' % {"username": message.chat.username})
     bot.send_message(message.chat.id, sentences[random.randint(0,len(sentences)-1)])
-    print(message)
 
 
 @bot.message_handler(func=lambda message: True, content_types=['voice'])
 def get_audio(message):
-    print(message)
+    #logging.info(message)
     if message.reply_to_message:
         f = bot.get_file(message.voice.file_id)
-        print(f)
         if message.voice.duration < 30:    
             wget.download('https://api.telegram.org/file/bot%(token)s/%(file_path)s' % {"token": token, "file_path": f.file_path}, path) 
-            logging.info('File %(file)s -> Phrase %(phrase)s' % {'file': f.file_path, 'phrase': message.reply_to_message.text})
+            logging.info('User %(username)s File %(file)s -> Phrase %(phrase)s' % {'username': message.from_user.username, 'file': f.file_path, 'phrase': message.reply_to_message.text})
             bot.send_message(message.chat.id, 'Спасибо! Чтобы спасти живой русский язык, еще разок введи команду /add_phrase')
         else:
             bot.send_message(message.chat.id, 'Ты что то очень долго рассказывал! Давай, чтобы спасти живой русский язык, еще разок введи команду /add_phrase')
